ed11d9c9cdeac33605e728dc74c48e43cdf55bef
Darthex <mrharshulsharma@gmail.com>
2025-05-07 08:35:27 +0530

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "@playwright/test"
 
 export default defineConfig({
-  testDir: '/.tests',
+  testDir: '/tests',
   snapshotDir: './.snapshots',
   use: {
     baseURL: 'http://localhost:6006',
