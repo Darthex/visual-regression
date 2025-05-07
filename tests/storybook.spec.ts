c7bb47afa7b1407f8bb16dcd0a5f1dc1719b70fa
Darthex <mrharshulsharma@gmail.com>
2025-05-07 08:00:55 +0530

    fix

---
+++ tests/storybook.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+const STORYBOOK_URL = 'http://localhost:6006';
+
+test.describe('Visual Regression for All Stories', () => {
+  let storyIds: string[] = [];
+
+  // @ts-ignore
+  test.beforeAll(async () => {
+    const res = await fetch(`${STORYBOOK_URL}/stories.json`);
+    const data = await res.json();
+    storyIds = Object.keys(data.entries);
+  });
+
+  for (const id of storyIds) {
+    // @ts-ignore
+    test(async ({ page }) => {
+      await page.goto(`${STORYBOOK_URL}/iframe.html?id=${id}`);
+      await expect(page).toHaveScreenshot(`${id}.png`);
+    });
+  }
+});
